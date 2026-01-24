94b36bcdd906aac6e5c6fa854bf2db24624c9f15
sebastianroman16 <s.gonzalezroman16@gmail.com>
2026-01-24 20:48:07 -0300

    feat: se modifica tabicon y aboutme

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardPresentation } from './components/card-presentation/card-presentation';
+import { PreloaderComponent } from './components/preloader/preloader.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CardPresentation],
+  imports: [CardPresentation, PreloaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  readonly isLoading = signal(true);
+
+  constructor() {
+    const hideLoader = () => this.isLoading.set(false);
+
+    if (document.readyState === 'complete') {
+      hideLoader();
+    } else {
+      window.addEventListener('load', hideLoader, { once: true });
+    }
+  }
 }
---
+++ src/app/components/preloader/preloader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preloader',
+  standalone: true,
+  templateUrl: './preloader.html',
+})
+export class PreloaderComponent {}
