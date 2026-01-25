dc7e9050e69cc4a523bf7af5bc62e2e9e4380a24
sebastianroman16 <s.gonzalezroman16@gmail.com>
2026-01-24 21:03:11 -0300

    feat: se modifica orden de diploma

--- src/app/components/sections/diplomas-section/diplomas-section.component.ts
+++ src/app/components/sections/diplomas-section/diplomas-section.component.ts
@@ -17,12 +17,12 @@ export class DiplomasSectionComponent {
 
   constructor(private sanitizer: DomSanitizer) {
     this.diplomas = [
-      this.buildDiploma('High School Diploma - Middle Level Technician', '/PDFs/20260124172355.pdf'),
-      this.buildDiploma('High School Diploma - English Translation', '/PDFs/20260124165817.pdf'),
+      this.buildDiploma('HVAC Technician Diploma', '/PDFs/20260124172758.pdf'),
+      this.buildDiploma('Valedictorian Diploma', '/PDFs/20260124172510.pdf'),
       this.buildDiploma('Industrial Automation and Control Technician', '/PDFs/20260124170757.pdf'),
       this.buildDiploma('Technician Diploma - English Translation', '/PDFs/20260124170018.pdf'),
-      this.buildDiploma('Valedictorian Diploma', '/PDFs/20260124172510.pdf'),
-      this.buildDiploma('HVAC Technician Diploma', '/PDFs/20260124172758.pdf'),
+      this.buildDiploma('High School Diploma - Middle Level Technician', '/PDFs/20260124172355.pdf'),
+      this.buildDiploma('High School Diploma - English Translation', '/PDFs/20260124165817.pdf'),
     ];
   }
 
